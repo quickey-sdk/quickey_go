6cfdcc8381c290b11542da94292e1375b8b4e8b9
quickey-sdk <86222272+quickey-sdk@users.noreply.github.com>
2021-07-30 14:31:22 +0700

    Add files via upload

---
+++ app.go
@@ -0,0 +1,72 @@
+package quickey
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func (q *Response) GetMetadata() *App {
+
+	values := map[string]string{"apiKey": q.ApiKey}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	responseJSON, err := http.Post(q.BaseUrl+"/auth/apiKey", "application/json",
+		bytes.NewBuffer(json_data))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var responseMap map[string]interface{}
+	json.NewDecoder(responseJSON.Body).Decode(&responseMap)
+
+	responseBytes, err := json.Marshal(responseMap["app"])
+
+	responseString := string(responseBytes)
+
+	app := App{}
+	json.Unmarshal([]byte(responseString), &app)
+	q.App = append(q.App, app)
+
+	return &app
+}
+
+func (q *Response) SendSMSOTP(phone string, provider string, appId string) *Customer {
+
+	values := map[string]string{"phone": phone, "provider": provider}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request, err := http.NewRequest("POST", q.BaseUrl+"/getSMSOTP/"+appId, bytes.NewBuffer(json_data))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("authorization", q.ApiKey)
+
+	client := &http.Client{}
+	responseJSON, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer responseJSON.Body.Close()
+
+	var responseMap map[string]interface{}
+	json.NewDecoder(responseJSON.Body).Decode(&responseMap)
+
+	responseBytes, err := json.Marshal(responseMap)
+
+	responseString := string(responseBytes)
+
+	customer := Customer{}
+	json.Unmarshal([]byte(responseString), &customer)
+	q.Customer = append(q.Customer, customer)
+
+	return &customer
+}
---
+++ auth.go
@@ -0,0 +1,112 @@
+package quickey
+
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func (q *Response) GetAccessTokenByEmail(email string, provider string) *Auth {
+	values := map[string]string{"email": email, "provider": provider}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request, err := http.NewRequest("POST", q.BaseUrl+"/loginCustomer", bytes.NewBuffer(json_data))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("authorization", q.ApiKey)
+
+	client := &http.Client{}
+	responseJSON, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer responseJSON.Body.Close()
+
+	var responseMap map[string]interface{}
+	json.NewDecoder(responseJSON.Body).Decode(&responseMap)
+
+	responseBytes, err := json.Marshal(responseMap)
+
+	responseString := string(responseBytes)
+
+	auth := Auth{}
+	json.Unmarshal([]byte(responseString), &auth)
+	q.Auth = append(q.Auth, auth)
+
+	return &auth
+}
+
+func (q *Response) GetAccessTokenByPhone(phone string, provider string) *Auth {
+
+	values := map[string]string{"phone": phone, "provider": provider}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request, err := http.NewRequest("POST", q.BaseUrl+"/loginCustomer", bytes.NewBuffer(json_data))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("authorization", q.ApiKey)
+
+	client := &http.Client{}
+	responseJSON, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer responseJSON.Body.Close()
+
+	var responseMap map[string]interface{}
+	json.NewDecoder(responseJSON.Body).Decode(&responseMap)
+
+	responseBytes, err := json.Marshal(responseMap)
+
+	responseString := string(responseBytes)
+
+	auth := Auth{}
+	json.Unmarshal([]byte(responseString), &auth)
+	q.Auth = append(q.Auth, auth)
+
+	return &auth
+}
+
+func (q *Response) linkPhoneToEmail(phone string, token string) *Customer {
+
+	values := map[string]string{"phone": phone, "token": token}
+	json_data, err := json.Marshal(values)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	request, err := http.NewRequest("POST", q.BaseUrl+"/otp/linkToEmail", bytes.NewBuffer(json_data))
+	request.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	request.Header.Set("authorization", q.ApiKey)
+
+	client := &http.Client{}
+	responseJSON, err := client.Do(request)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer responseJSON.Body.Close()
+
+	var responseMap map[string]interface{}
+	json.NewDecoder(responseJSON.Body).Decode(&responseMap)
+
+	responseBytes, err := json.Marshal(responseMap)
+
+	responseString := string(responseBytes)
+
+	customer := Customer{}
+	json.Unmarshal([]byte(responseString), &customer)
+	q.Customer = append(q.Customer, customer)
+
+	return &customer
+}
